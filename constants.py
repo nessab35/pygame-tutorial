962bed31b544f4ab2a10730f2fa881bdcb519d46
nessab35 <nessa.begay@students.dominican.edu>
2023-11-17 14:24:46 -0800

    completed pygame tutorial

--- constants.py
+++ constants.py
@@ -1,4 +1,11 @@
+import pygame
+pygame.font.init()
+
 lanes = [93, 218, 343]
 PINK = (199, 70, 177)
 BLACK = (0, 0, 0)
 fruit_captured = 0
+score_increase = 100
+my_font = pygame.font.SysFont('timesnewroman', 20, True)
+my_font_scoreboard = pygame.font.SysFont('timesnewroman', 38, True)
+screen = pygame.display.set_mode([500, 500])
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.locals import *
 from apple import Apple
 from strawberry import StrawBerry
 from player import Player
@@ -7,11 +6,12 @@ from bomb import Bomb
 from alian import Alian1, Alian2
 from cloud import Cloud1
 from bird import Bird
-from constants import PINK, BLACK, fruit_captured
-pygame.font.init()
+from scoreboard import ScoreBoard
+from constants import PINK, BLACK, fruit_captured, my_font, screen
 
-screen = pygame.display.set_mode([500, 500])
+
 clock = pygame.time.Clock()
+
 
 apple = Apple()
 strawberry = StrawBerry()
@@ -23,22 +23,26 @@ cloud_one = Cloud1()
 cloud_two = Cloud1()
 cloud_three = Cloud1()
 bird_ = Bird()
+score = ScoreBoard(45, 30, 0) # x = 45, y = 30 which are the postions of the scoreboard
+
 
 # made sprites Group
 fruit_sprites = pygame.sprite.Group()
 fruit_sprites.add(apple)
 fruit_sprites.add(strawberry)
 
+
 alian_sprites = pygame.sprite.Group()
 alian_sprites.add(alian_one)
 alian_sprites.add(alian_two)
+
 
 bomb_sprites = pygame.sprite.Group()
 bomb_sprites.add(bomb)
 
 
 all_sprites = pygame.sprite.Group()
-sprites_to_add = [player, apple, strawberry, bomb, alian_one, alian_two, cloud_one, cloud_two, cloud_three, bird_]
+sprites_to_add = [player, apple, strawberry, bomb, alian_one, alian_two, cloud_one, cloud_two, cloud_three, bird_, score]
 for sprite in sprites_to_add:
     all_sprites.add(sprite)
 
@@ -58,13 +62,12 @@ def play_sound(sound):
     return sound
 
 
-def show_game_over():
+def show_game_over(score):
     screen.fill((BLACK))
-    my_font = pygame.font.SysFont('timesnewroman', 20)
-    line_1 = my_font.render("Game Over! Your score is ____", True, (PINK))
-    screen.blit(line_1, (65, 150))
+    line_1 = my_font.render(f"Game Over! Your score is {score}", True, (PINK))
+    screen.blit(line_1, (60, 200))
     line_2 = my_font.render("To play again, press ENTER. To exit, press ESC!", True, (PINK))
-    screen.blit(line_2, (70, 200))
+    screen.blit(line_2, (60, 250))
 
     pygame.display.flip()
 
@@ -96,6 +99,7 @@ while running:
                 apple.reset()
                 alian_one.reset()
                 alian_two.reset()
+                score.reset()
             elif event.key == pygame.K_LEFT:
                 player.left()
             elif event.key == pygame.K_RIGHT:
@@ -114,8 +118,10 @@ while running:
         entity.render(screen)
         fruit = pygame.sprite.spritecollideany(player, fruit_sprites)
         alian = pygame.sprite.spritecollideany(player, alian_sprites)
+
         if fruit:
             play_sound("eating_fruit")
+            score.update()
             fruit_captured += 0.3
             fruit.reset()
 
@@ -127,10 +133,13 @@ while running:
 
         if pygame.sprite.collide_rect(player, bomb):
             play_sound("explosion")
+            alian_one.reset()
+            alian_two.reset()
             game_over = True
 
+
     if game_over:
-        show_game_over()
+        show_game_over(score.score)
 
     # update the window
     pygame.display.flip()
